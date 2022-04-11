9c2bc62dd5813ff19def9d1cd023afeb8d819b43
ali-kmirzaei <77521376+ali-kmirzaei@users.noreply.github.com>
2022-04-11 00:27:21 -0700

    Rename degree to angle

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
+# import timeit
+# start = timeit.default_timer()
+
 import TextDetection as td
 import BalanceDegree as bd
 import cv2 as cv
 
+
+
 def main():
     filename = '02.jpeg'
-    src = cv.imread(filename)
+    img = cv.imread(filename)
 
     while cv.waitKey(1) != ord('0'):
-        cv.imshow("Rotated", src)
+        cv.imshow("Rotated", img)
 
-    degree = bd.find_degree(src)
-    if type(degree) == str: 
-        print(degree)   
+    angle = bd.find_degree(img)
+    print(angle)
+    if type(angle) == str:
+        print(angle)
         return 0
 
-    img = bd.rotate(src, degree)
+    img = bd.rotate(img, angle)
+
+    while cv.waitKey(1) != ord('0'):
+        cv.imshow("Rotated", img)
+
     td.test_single_img(img)
     return 1
 
 
 if __name__ == '__main__' :
     print(main())
+
+
+
+# stop = timeit.default_timer()
+# print('Time: ', stop - start)
